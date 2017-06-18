98056c679e967635ad674433deee1df619714375
dennist <dennis.theotokatos@kununu.com>
2017-06-18 17:50:27 +0200

    replace dts-generator with tsc for declarations

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,12 @@
-declare module 'dyna-ts-module-boilerplate/index' {
-	export interface IPerson {
-	    name: string;
-	    age: number;
-	}
-	export class Person {
-	    private name;
-	    private age;
-	    constructor(name: string, age: number);
-	    getName(): string;
-	    getAge(): number;
-	    get(): IPerson;
-	}
-
+export interface IPerson {
+    name: string;
+    age: number;
+}
+export declare class Person {
+    private name;
+    private age;
+    constructor(name: string, age: number);
+    getName(): string;
+    getAge(): number;
+    get(): IPerson;
 }
