199ef5f318989694317946c4eef29046b493887d
dennis7 <dynasdaq@gmail.com>
2017-09-03 09:06:54 +0200

    webpack, ignore node_modules

--- webpack.debug.nodejs.config.js
+++ webpack.debug.nodejs.config.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
 
 const loaders = require('./webpack.loaders');
 const plugins = require('./webpack.plugins');
@@ -18,6 +19,7 @@ const config = {
     // the entry application code
     path.resolve(__dirname, 'debug/index.ts')
   ],
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   output: {
     path: path.resolve(__dirname, 'debug-ground/debug-on-nodejs'),
     filename: 'index.js'
