e66d451dd17c3010a46dceba4420414f36642e23
dennis7 <dynasdaq@gmail.com>
2017-09-22 07:10:05 +0200

    test impr

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,13 +1,13 @@
 declare let jasmine: any, describe:any, expect:any, it: any;
 
-const ITEM_TIMEOUT_MS: number = 150;
+const STRESS_TEST: boolean = true;
+const ITEM_TIMEOUT_MS: number = 10;
+const NORMAL_ITEMS_COUNT: number = 20;
+const STRESS_ITEMS_COUNT: number = 200;
 
-const STRESS_TEST: boolean = false;
-const STRESS_ITEMS_COUNT: number = 2000;
-const ITEMS_COUNT: number = STRESS_TEST && STRESS_ITEMS_COUNT || 1;
-
-if (typeof jasmine !== 'undefined')
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = (STRESS_TEST && STRESS_ITEMS_COUNT || 1) * ITEM_TIMEOUT_MS;
+const ITEMS_COUNT: number = STRESS_TEST && STRESS_ITEMS_COUNT || NORMAL_ITEMS_COUNT;
+const timeout = (STRESS_TEST && STRESS_ITEMS_COUNT || 1) * ITEM_TIMEOUT_MS;
+if (typeof jasmine !== 'undefined') jasmine.DEFAULT_TIMEOUT_INTERVAL = timeout;
 
 import {Person} from '../src';
 
