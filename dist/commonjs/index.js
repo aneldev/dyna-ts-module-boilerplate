3159eb88cdc55421f03554724b69b389bdbedf35
dennisat <info@anel.co>
2019-01-21 11:24:59 +0100

    impl: pure exports

---
+++ dist/commonjs/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+// Export always from here for the types
+__export(require("./person/Person"));
+console.error("\nmy-module: Import error\n    You should import \"my-module/web\" or \"my-module/node\" (with lazy  or not) according the runtime environment.\n    For typescript, you should import the types from \"my-module\" but functional code from web or node versions.\n    More for how to import with conditional lazy load: https://github.com/aneldev/dyna-ts-module-boilerplate#how-to-import\n");
+//# sourceMappingURL=index.js.map
