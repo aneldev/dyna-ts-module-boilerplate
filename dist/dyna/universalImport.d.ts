cfad4840e6daa61bc8b23d44b5f1acdc7dc08ec7
dennisat <info@anel.co>
2019-01-21 11:25:07 +0100

    impl: build

--- dist/dyna/universalImport.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const importUniversal: <TModule>(moduleName: string) => TModule;
-export declare const exportNode: (references: {
-    [refName: string]: any;
-}) => void;
-export declare const exportWeb: (references: {
-    [refName: string]: any;
-}) => void;
