da90017d9c9817c6895ab72355b5c6cdf13e3015
dennisat <info@anel.co>
2019-01-27 20:33:27 +0100

    fix: types in deleteTempFolder

--- dyna/deleteTempFolder.ts
+++ dyna/deleteTempFolder.ts
@@ -2,7 +2,7 @@ const execSync = require('child_process').execSync;
 
 export const deleteTempFolder = (): void => {
 	console.log('Deleting temp folder...');
-	execSync("rm -rf ./temp", function (err, stdout) {
+	execSync("rm -rf ./temp", function (err: Error, stdout: string) {
 		console.log(stdout);
 		if (err) console.error('Error, cannot clear the temp folder', err);
 	});
