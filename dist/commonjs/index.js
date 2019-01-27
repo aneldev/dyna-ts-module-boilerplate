d438e79ed2d3409a8f369557e2dbf61d536c0c94
dennisat <info@anel.co>
2019-01-27 11:06:43 +0100

    refactor: rename path commonjs to commonJs

--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // Export always from here for the types
 var Person_1 = require("./person/Person");
 exports.Person = Person_1.Person;
-console.error("\nmy-module: Import error\n    You should import \"my-module/web\" or \"my-module/node\" (with lazy  or not) according the runtime environment.\n    For typescript, you should import the types from \"my-module\" but functional code from web or node versions.\n    More for how to import with conditional lazy load: https://github.com/aneldev/dyna-ts-module-boilerplate#how-to-import\n");
+console.error("\nmy-module: Import error\n    You should import \"my-module/web\" or \"my-module/node\" (with lazy load or not) according the runtime environment.\n    For typescript, you should import the types from \"my-module\" but functional code from \"web\" or \"node\" versions.\n    More for how to import with conditional lazy load: https://github.com/aneldev/dyna-ts-module-boilerplate#how-to-import\n");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export {
 
 console.error(`
 my-module: Import error
-    You should import "my-module/web" or "my-module/node" (with lazy  or not) according the runtime environment.
-    For typescript, you should import the types from "my-module" but functional code from web or node versions.
+    You should import "my-module/web" or "my-module/node" (with lazy load or not) according the runtime environment.
+    For typescript, you should import the types from "my-module" but functional code from "web" or "node" versions.
     More for how to import with conditional lazy load: https://github.com/aneldev/dyna-ts-module-boilerplate#how-to-import
 `);
