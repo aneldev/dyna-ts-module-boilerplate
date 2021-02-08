431440f582754e2fde4f41f7f656cc5779e6152f
Dennis <dennisat@users.noreply.github.com>
2021-02-08 13:18:52 +0100

    Build changes

--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -4,5 +4,5 @@ exports.Person = void 0;
 // Export always from here for the types
 var Person_1 = require("./person/Person");
 Object.defineProperty(exports, "Person", { enumerable: true, get: function () { return Person_1.Person; } });
-console.error("\nmy-module: Import error\n    You should import \"my-module/dist/commonJs/web\" or \"my-module/dist/commonJs/node\" (with lazy load or not) according the runtime environment.\n    There is also am es version instead of commonJs, there imports are \"my-module/dist/esNext/web\" and \"my-module/dist/esNext/node\".\n    For typescript, you are free to import the types from \"my-module\" but functional code from \"web\" or \"node\" versions.\n");
+console.error("\nmy-module: Import error\n    You should import \"my-module/dist/commonJs/web\" or \"my-module/dist/commonJs/node\" (with lazy load or not) according the runtime environment.\n    There is also an \"es\" version instead of \"commonJs\", there imports are \"my-module/dist/esNext/web\" and \"my-module/dist/esNext/node\".\n    For typescript, you are free to import the types from \"my-module\" but functional code from \"web\" or \"node\" versions.\n");
 //# sourceMappingURL=index.js.map
