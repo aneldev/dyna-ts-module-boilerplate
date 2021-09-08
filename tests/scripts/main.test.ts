dc896395f8f77939525191766079a91430b0d867
dennisat <dennis@anel.co>
2021-09-08 19:32:25 +0200

    Upgrade to eslint

--- tests/scripts/main.test.ts
+++ tests/scripts/main.test.ts
@@ -1,7 +1,7 @@
 import "jest";
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 4000;
 
-import {Person} from "../../src/node"
+import {Person} from "../../src/node";
 
 // help: https://facebook.github.io/jest/docs/expect.html
 
@@ -10,5 +10,5 @@ describe('Internal module test', () => {
     const n = new Person("John", 32);
     expect(n.getName()).toBe("John");
     expect(n.getAge()).toBe(32);
-	});
+  });
 });
