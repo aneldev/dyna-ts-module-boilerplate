8c24ebc4092c7398f8845845706569a1fbfd55f6
dennisat <dennis@anel.co>
2024-09-02 14:45:59 +0200

    Impr: test

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ import "./utils/unhandledPromiseRejectionsInNode";
 
 import * as jsonData from "./test.json";
 
-export const sayHelloWorld = () => console.log('sayHelloWorld(): Hello world', {jsonData});
+export const sayHelloWorld = (): Record<string, any> => {
+  console.log('sayHelloWorld(): Hello world', {jsonData});
+  return jsonData;
+};
---
+++ tests/scripts/main.test.ts
@@ -0,0 +1,8 @@
+// Help: https://facebook.github.io/jest/docs/expect.html
+import {sayHelloWorld} from "../../src";
+
+describe('Unit test', () => {
+  it('Sample', () => {
+    expect(sayHelloWorld()).toMatchSnapshot();
+  });
+});
