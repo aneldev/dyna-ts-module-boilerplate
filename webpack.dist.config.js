cab5d85ff48866b09fc77bc29d15d785aca3ff2e
dennisat <dennis@anel.co>
2024-12-28 13:20:45 +0100

    Cleanup unused deps

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -1,6 +1,7 @@
 ﻿const fs = require('fs');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const buffer = require.resolve("buffer");
 
 const isSingleModule =
   fs.existsSync('./src/index.ts') ||
@@ -16,8 +17,8 @@ const plugins = require('./webpack.plugins.js');
  */
 const EXCLUDE_SRC_FOLDERS = [
   "@types",
-  // Other folders that won't by built by Webpack might be listed here
-]
+  // Other folders that won't be built by Webpack might be listed here
+];
 
 const getModuleNames =
   root =>
@@ -82,7 +83,7 @@ module.exports = {
     alias: {},
     extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".jsx"],
     fallback: {
-      stream: require.resolve("buffer/"),
+      stream: buffer,
     }
   },
   module: {
